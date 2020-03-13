4bd3350a393c75a6e573a3e2918a71fde2d2978e
Yota.K <powdersugar828828@gmail.com>
2020-03-13 18:54:29 +0900

    カスタマイザーから色を変更する機能を追加。

--- inc/customizer.php
+++ inc/customizer.php
@@ -2,7 +2,7 @@
 
 // カスタマイザー 
 function penguin_media_customize_register( $wp_customize ) {
-	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogname' )->transport = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 
@@ -17,7 +17,90 @@ function penguin_media_customize_register( $wp_customize ) {
 		) );
 	}
 }
-add_action( 'customize_register', 'penguin_media_customize_register' );
+add_action('customize_register', 'penguin_media_customize_register');
+
+// 色の設定
+function penguin_media_color_customize_register( $wp_customize ) {
+    $settings_ary = array(
+        'setting_1' => array(
+            'setting' => 'header_bg_setting',
+            'default' => '#fff',
+            'label_name' => 'ヘッダーの背景色',
+            'priority' => 1,
+        ),
+        'setting_2' => array(
+            'setting' => 'key_color_1',
+            'default' => '#6262f7',
+            'label_name' => 'キーカラー1',
+            'priority' => 20,
+        ),
+        'setting_3' => array(
+            'setting' => 'key_color_2',
+            'default' => '#ccf9f1',
+            'label_name' => 'キーカラー2',
+            'priority' => 20,
+        ),
+    );
+
+    foreach($settings_ary as $key => $setting) {
+        $wp_customize->add_setting($setting['setting'], array(
+            'default' => $setting['default']
+        ));
+        $wp_customize->add_control(
+            new WP_Customize_Color_Control(
+                $wp_customize,
+                $setting['setting'],
+                array(
+                    'label' => $setting['label_name'],
+                    'section' => 'colors',
+                    'settings' => $setting['setting'],
+                    'priority' => $setting['priority'],
+                )
+            )
+        );
+    };
+
+}
+add_action('customize_register', 'penguin_media_color_customize_register');
+
+// 色の反映
+function customizer_color() {
+    ?>
+    <style>
+    #header {
+        background: <?php echo get_theme_mod('header_bg_setting'); ?>;
+    }
+
+    .widget h2, #top-slider .swiper-container .swiper-pagination-bullets .swiper-pagination-bullet-active {
+        border-color: <?php echo get_theme_mod('key_color_1'); ?>;
+    }
+
+    #top-slider .swiper-container .swiper-wrapper .swiper-slide .slide-container .swiper-txt h2 a {
+        background: linear-gradient(to top, <?php echo get_theme_mod('key_color_1'); ?>, 4px, #fff 4px);
+    }
+
+    #top-slider .swiper-container .swiper-pagination-bullets .swiper-pagination-bullet-active, 
+    #top-slider .swiper-container .swiper-wrapper .swiper-slide .slide-container .swiper-txt .post-info a:hover,
+    #main .tagcloud a:hover,
+    .post-wrapper .post-header .post-info .post-category a:hover,
+    #grobal-nav .widget-title,
+    #grobal-nav .menu-title,
+    #grobal-nav .nav-wrapper .nav-container #site-navigation .search-form .search-submit
+    {
+        background: <?php echo get_theme_mod('key_color_1'); ?>;
+    }
+
+    .post-wrapper #content .entry-content .toc-container {
+        border-top-color: <?php echo get_theme_mod('key_color_1'); ?>;
+    }
+
+    #main .post-list .post-box:hover {
+        background: <?php echo get_theme_mod('key_color_2'); ?>;
+    }
+    </style>
+    <?php
+}
+add_action('wp_head', 'customizer_color');
 
 function penguin_media_customize_partial_blogname() {
 	bloginfo( 'name' );
